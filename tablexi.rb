938045f347249a7a551dec098a25fca9d40fedc3
davidwkaiser <davidwkaiser@yahoo.com>
2016-06-16 14:50:42 -0500

    remove dupes

--- tablexi.rb
+++ tablexi.rb
@@ -34,7 +34,7 @@ menu.load_data(DATA)
 # x =  menu.build_a_set_of_orders(7)
 # p x[0]
 # p menu.add_up?(x[0])
-x = menu.solve
+x = menu.solve_without_dupes
 x.each do |variant|
   p variant
   p "***"
